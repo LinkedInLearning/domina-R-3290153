ca52e89f2f9c372c8b192e9cef14fe92451490d3
Lincy <lincgr@gmail.com>
2023-06-17 11:06:18 -0600

    01_05 Verificar si dos dataframes son iguales en R

---
+++ 01_05/01_05.r
@@ -0,0 +1,12 @@
+dataframe1 <- data.frame (
+  Nombre = c("Luis", "Ana"),
+  Edad = c(15, 30)
+)
+print(dataframe1)
+
+dataframe2 <- data.frame(
+  Nombre = c("Pedro", "Luisa"),
+  Edad = c(28, 35),
+  Ciudad = c("Naranjo", "Grecia")
+)
+print(dataframe2)
