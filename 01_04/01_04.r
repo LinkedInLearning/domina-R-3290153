5dba151394d5b12a18f4d0600d4345d5a8a6e967
Lincy <lincgr@gmail.com>
2023-06-17 09:50:15 -0600

    01_04 Comparar dos listas en R

---
+++ 01_04/01_04.r
@@ -0,0 +1,15 @@
+#1
+lista1 <- list(1,2,3)
+lista2 <- list(2,3,1)
+
+#2
+lista1 <- list(1,2,3)
+lista2 <- list(1,2,3)
+
+#3
+lista1 <- list(1,2,3)
+lista2 <- list(2,3,1)
+
+#4
+lista1 <- list(a = 1, b = "two", c = 3)
+lista2 <- list(a = 1, b = "two", c = 4)
