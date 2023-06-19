7486a5ba08b24cc3fcd07eec6705dbc3bf243635
Lincy <lincgr@gmail.com>
2023-06-19 15:36:22 -0600

    02_05 Crear uniones de dos dataframe en R

---
+++ 02_05/02_05.r
@@ -0,0 +1,14 @@
+dataframe1 <- data.frame (
+  cliente = c("Zenith", "Veloz", "Lumina", "Aurora", "Spectra", "Electra", "Vortrix", "Orion", "Nimbus"),
+  cantidadcolaboradores = c(350, 300, 320, 41, 15, 100, 84, 180, 117)
+)
+
+print(dataframe1)
+
+dataframe2 <- data.frame (
+  cliente = c("Polaris", "Cortex", "Aether", "Zephyr", "Infinito", "Celesto", "Eureka", "Fusion", "Prisma"),
+  cantidadcolaboradores = c(65, 234, 123, 94, 150, 94, 23, 350, 110)
+)
+
+print(dataframe2)
+
