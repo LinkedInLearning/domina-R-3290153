386c770291f96d2a4f2f23eeccc70a6f58b05c9f
Lincy <lincgr@gmail.com>
2023-06-19 15:28:27 -0600

    02_03 Extraer una columna específica de un dataframe en R

---
+++ 02_03/02_03.r
@@ -0,0 +1,13 @@
+dataframe1 <- data.frame (
+  nombre = c("Luis", "Ana", "Sebastián", "Sofía", "Mateo", "Camila", "Valentín", "Isabella","Lucía", "Diego", "Valeria"),
+  edad = c(15, 30, 32, 41, 47, 23, 20, 18, 17, 25, 14), 
+  ciudad = c("Buenos Aires", "Lima", "Ciudad de México", "Santiago", "Alajuela", "Nueva York", "Toronto", "Río de Janeiro", "Montevideo", "Miami", "Vancouver")
+)
+
+print(dataframe1)
+
+edad <- dataframe1$edad
+print(edad)
+
+edad <- dataframe1[, 2]
+print(edad)
