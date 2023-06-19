cc369742f7440dce15a4cad20e298c8dae963a2f
Lincy <lincgr@gmail.com>
2023-06-19 15:35:42 -0600

    02_04 Agregar una fila y una columna a un dataframe en R

---
+++ 02_04/02_04.r
@@ -0,0 +1,14 @@
+dataframe <- data.frame (
+  nombre = c("Luis", "Ana", "Sebastián", "Sofía", "Mateo", "Camila", "Valentín", "Isabella","Lucía", "Diego", "Valeria"),
+  edad = c(35, 30, 32, 41, 47, 23, 20, 18, 17, 25, 14), 
+  ciudad = c("Buenos Aires", "Lima", "Ciudad de México", "Santiago", "Alajuela", "Nueva York", "Toronto", "Río de Janeiro", "Montevideo", "Miami", "Vancouver")
+)
+print(dataframe)
+
+nuevo_registro <- data.frame(nombre = "Guillermo", edad = 28, ciudad = "Cartago")
+dataframe <- rbind(dataframe, nuevo_registro)
+print(dataframe)
+
+nueva_columna <- c("Médico", "Ingeniera civil", "Abogado", "Profesora", "Contador", "Enfermera", "Estudiante", "Periodista", "Programadora de software", "Gerente de ventas", "Estudiante", "Psicólogo")
+dataframe <- cbind(dataframe, profesión = nueva_columna)
+print(dataframe)
